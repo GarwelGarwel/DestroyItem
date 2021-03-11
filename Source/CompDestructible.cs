b75fd74e15f4d7a199984afbd1a2c997098d94f0
GarwelGarwel <okozlovsky@gmail.com>
2021-03-11 19:36:07 +0300

    Added Destroy Item gizmo

--- Source/CompDestructible.cs
+++ Source/CompDestructible.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
+using UnityEngine;
 using Verse;
 
 namespace DestroyItem
 {
     public class CompDestructible : ThingComp
     {
-        public override IEnumerable<Gizmo> CompGetGizmosExtra() => base.CompGetGizmosExtra();
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            yield return new Command_Action
+            {
+                defaultLabel = "Destroy",
+                defaultDesc = "Order a pawn to destroy this item",
+                icon = ContentFinder<Texture2D>.Get("DestroyItem"),
+                action = () => parent.Destroy()
+            };
+        }
     }
 }
