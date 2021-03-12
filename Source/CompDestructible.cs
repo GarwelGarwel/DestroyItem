084db7a434b8b08bbfb9e2971362478e68d301a6
GarwelGarwel <okozlovsky@gmail.com>
2021-03-12 10:14:21 +0300

    Added Architect tab order, mouse icon, sounds

--- Source/CompDestructible.cs
+++ Source/CompDestructible.cs
@@ -8,6 +8,8 @@ namespace DestroyItem
     {
         public override IEnumerable<Gizmo> CompGetGizmosExtra()
         {
+            if (parent.Map.designationManager.DesignationOn(parent, DestroyItemDefOf.Designation_DestroyItem) != null)
+                yield break;
             yield return new Command_Action
             {
                 defaultLabel = "Destroy",
--- Source/Designator_DestroyItem.cs
+++ Source/Designator_DestroyItem.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace DestroyItem
@@ -12,22 +13,24 @@ namespace DestroyItem
             defaultLabel = "Destroy Item";
             defaultDesc = "Order a pawn to destroy this item";
             icon = ContentFinder<Texture2D>.Get("Command");
+            useMouseIcon = true;
+            soundDragSustain = SoundDefOf.Designate_DragStandard;
+            soundDragChanged = SoundDefOf.Designate_DragStandard_Changed;
+            soundSucceeded = SoundDefOf.Designate_Deconstruct;
         }
 
         protected override DesignationDef Designation => DestroyItemDefOf.Designation_DestroyItem;
 
         public override AcceptanceReport CanDesignateCell(IntVec3 loc)
         {
-            if (!loc.InBounds(Map))
-                return false;
-            if (!DebugSettings.godMode && loc.Fogged(Map))
+            if (!loc.InBounds(Map) || !DebugSettings.godMode && loc.Fogged(Map))
                 return false;
             return DestructibleInCell(loc) != null;
         }
 
         public override void DesignateSingleCell(IntVec3 c) => DesignateThing(DestructibleInCell(c));
 
-        public override AcceptanceReport CanDesignateThing(Thing t) => t.def.HasComp(typeof(CompDestructible));// && (t.Faction == null || t.Faction.IsPlayer);
+        public override AcceptanceReport CanDesignateThing(Thing t) => t.def.HasComp(typeof(CompDestructible)) && Map.designationManager.DesignationOn(t, Designation) == null;// && (t.Faction == null || t.Faction.IsPlayer);
 
         public override void DesignateThing(Thing t)
         {
@@ -35,18 +38,6 @@ namespace DestroyItem
             Map.designationManager.AddDesignation(new Designation(t, Designation));
         }
 
-        Thing DestructibleInCell(IntVec3 loc)
-        {
-            foreach (Thing thing in loc.GetThingList(Map))
-            {
-                //Utility.Log($"Thing {thing.LabelCap} is at {loc}.");
-                if (CanDesignateThing(thing).Accepted && Map.designationManager.DesignationOn(thing, Designation) == null)
-                {
-                    //Utility.Log($"{thing.LabelCap} @ {loc} is destructible.");
-                    return thing;
-                }
-            }
-            return null;
-        }
+        Thing DestructibleInCell(IntVec3 loc) => loc.GetThingList(Map).FirstOrFallback(thing => CanDesignateThing(thing).Accepted);
     }
 }
