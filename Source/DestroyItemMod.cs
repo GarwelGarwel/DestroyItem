d81e91f86e2ecef10392d9674753577f904d0e99
GarwelGarwel <okozlovsky@gmail.com>
2021-08-09 08:01:17 +0300

    Fixed settings being reset on load

--- Source/DestroyItemMod.cs
+++ Source/DestroyItemMod.cs
@@ -10,17 +10,22 @@ namespace DestroyItem
 
         public DestroyItemMod(ModContentPack content)
             : base(content)
-            => destructionSpeedPower = (float)Math.Round(Mathf.Log10(Settings.destructionSpeed), 1);
+        {
+            GetSettings<Settings>();
+            destructionSpeedPower = (float)Math.Round(Mathf.Log10(Settings.destructionSpeed), 1);
+        }
 
-        public override void DoSettingsWindowContents(Rect inRect)
+        public override void DoSettingsWindowContents(Rect rect)
         {
             Listing_Standard content = new Listing_Standard();
-            content.Begin(inRect);
+            content.Begin(rect);
             content.CheckboxLabeled("Instant destruction", ref Settings.instantDestruction, "Destroy all items instantly.");
             if (!Settings.instantDestruction)
             {
-                content.Label($"Destruction Speed: {Settings.destructionSpeed.ToStringPercent()}", tooltip: "Relative speed of item destruction. Actual speed depends on the pawn's melee damage and general labor speed.");
-                destructionSpeedPower = (float)Math.Round(content.Slider(destructionSpeedPower, -1, 1), 1);
+                content.Label(
+                    $"Destruction Speed: {Settings.destructionSpeed.ToStringPercent()}",
+                    tooltip: "Relative speed of item destruction. Actual speed depends on the pawn's melee damage and general labor speed.");
+                destructionSpeedPower = (float)Math.Round(content.Slider(destructionSpeedPower, -1, 1), 2);
                 Settings.destructionSpeed = Mathf.Pow(10, destructionSpeedPower);
             }
             content.End();
